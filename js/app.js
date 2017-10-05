a0a35c8916e0cff375fc8e7a787faea328099c1b
Maria Lamardo <marialamardo@outlook.com>
2017-10-04 23:06:08 -0400

    track num clicks/matched tiles and end of game

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-/*
- * Create a list that holds all of your cards
- */
+//list holds all cards
 const deck = {
     cards: [
         {
@@ -86,44 +84,35 @@ const deck = {
     ]
 }
 
-
-//consoles all card names
-// for (let i = 0; i < deck.cards.length; i++) {
-//     console.log(deck.cards[i].name);
-//     console.log(deck.cards[i].icon);
-//     console.log(deck.cards[i].class);
-// }
-
-/*
-* Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
+// Shuffle cards
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
+    var currentIndex = array.length, temporaryValue, randomIndex;  
     // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
+    while (0 !== currentIndex) {  
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
+      currentIndex -= 1;  
       // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
-    }
-  
+    }  
     return array;
-  }
-  
+}  
 // Shuffle and log results
 shuffle(deck.cards);
 console.log(deck.cards);
+console.log(deck.cards.length);
+
+//variables
+//tiles that have open and show class
+let seenTiles = [];
+//number of tiles flipped in this round
+let tilesFlipped = 0;
+//track number of moves
+let numMoves = 0;
+//tiles matched
+let tilesMatched = [];
 
 // Loop through each card and create its HTML
 newGame = () => {
@@ -132,31 +121,22 @@ newGame = () => {
     for (let i = 0; i < deck.cards.length; i++) {
         output += `<li class="${deck.cards[i].class}" onclick="clickFunction(this)"><i class="${deck.cards[i].icon}"></i></li>`; 
     };
-    document.getElementById('deck').innerHTML = output;    
+    document.getElementById('deck').innerHTML = output;
+    document.getElementsByClassName('moves')[0].innerHTML = 0;
+    numMoves = 0;
+    seenTiles = [];
+    shuffle(deck.cards);
 }
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */ 
-
-//tiles that have open and show class
-let seenTiles = [];
-//number of tiles flipped in this round
-let tilesFlipped = 0;
 
 //Function when card is clicked
 clickFunction = (element) => {
+    //increase number of moves by 1 for every click
+    numMoves +=1;
+    document.getElementsByClassName('moves')[0].innerHTML = numMoves;
     //add 1 for every card flipped
     tilesFlipped += 1;
     //only allow flips if there are < or = 2 flipped cards and element class is not match
-    if (tilesFlipped <= 2){ 
+    if (tilesFlipped <= 2 ){ 
         //flip and show card
         element.setAttribute("class", "card open show");
         //add card's child element which hold the icon to seenTiles
@@ -170,9 +150,17 @@ clickFunction = (element) => {
             //remove open and show classes
             seenTiles[0].setAttribute("class", "card match");
             seenTiles[1].setAttribute("class", "card match");   
+            //add to tilesMatched
+            tilesMatched.push(seenTiles[0]);
+            tilesMatched.push(seenTiles[1]);
+            console.log(tilesMatched.length);
             //empty seenTiles array       
             seenTiles = [];
             tilesFlipped = 0;
+            if (tilesMatched.length == deck.cards.length){
+                alert("Congratulations!! You won!!!");
+                newGame();
+            }
         }        
         //if className(icons) do NOT match
         else if (seenTiles[0].children[0].className != seenTiles[1].children[0].className){            
@@ -184,8 +172,8 @@ clickFunction = (element) => {
                 seenTiles = [];            
             }, 500);            
             tilesFlipped = 0;
-        }
-    }
+        }        
+    }    
 }
 //add each card's HTML to the page
 newGame();
